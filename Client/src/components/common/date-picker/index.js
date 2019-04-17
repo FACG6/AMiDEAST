4ba9a4d05901a4444ef4279dd66f793f2e16de5e
Nareman <naremanmohhilles@gmail.com>
2019-04-17 16:11:37 +0300

    fix indentation

--- Client/src/components/common/date-picker/index.js
+++ Client/src/components/common/date-picker/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-function Datepicker(){
-return(
-<select>
-  <option value="0">Saturn's-Moon's-Woden's</option>
-  <option value="1">Sun's-Tiu's-Thor's</option>
-</select>
-);
+import React from "react";
+function Datepicker() {
+  return (
+    <select>
+      <option value="0">Sat - Mon - Wen</option>
+      <option value="1">Sun - Tues - Thurs </option>
+    </select>
+  );
 }
 export default Datepicker;
