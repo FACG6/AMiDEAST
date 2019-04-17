23cbe81bef53e35728b362cfc8fca8c69ebffc84
Nareman <naremanmohhilles@gmail.com>
2019-04-17 16:06:30 +0300

    edit endintation

---
+++ Client/src/components/common/date-picker/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Datepicker() {
+  return (
+    <select>
+      <option value="0">Saturday-Monday-Wednesday</option>
+      <option value="1">Sunday-Tuesday-Thursday</option>
+    </select>
+  );
+}
+export default Datepicker;
