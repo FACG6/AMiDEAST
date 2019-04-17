aa309d0a6d3ef028ed313fa68ae6a3f21a50357e
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-17 18:51:59 +0300

    edit the footer

--- Client/src/components/Layout/MobileFooter/index.js
+++ Client/src/components/Layout/MobileFooter/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      footer      
-    </div>
+    <div className="footer">
+      <i class="fab fa-facebook-f"></i>
+      <i class="fab fa-twitter"></i>
+      <i class="fab fa-instagram"></i>
+   </div>
   )
 }
