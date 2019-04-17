a227415676d4377ffcd90df8e13b93e8faf6cbdf
Nareman <naremanmohhilles@gmail.com>
2019-04-17 15:39:37 +0300

    date picker component relates #17

---
+++ Client/src/components/common/date-picker/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Datepicker(){
+return(
+<select>
+  <option value="0">Saturn's-Moon's-Woden's</option>
+  <option value="1">Sun's-Tiu's-Thor's</option>
+</select>
+);
+}
+export default Datepicker;
