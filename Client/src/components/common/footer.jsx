a2988517fa644d953be60a4cea215c3a817f8f08
Nareman <naremanmohhilles@gmail.com>
2019-04-17 13:44:03 +0300

    footer component Relates  #21

---
+++ Client/src/components/common/footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+function Footer(){
+    return(
+      <div className="footer">
+     <i class="fab fa-facebook-f"></i>
+     <i class="fab fa-twitter"></i>
+     <i class="fab fa-instagram"></i>
+    </div>
+  );
+  
+}
+
+export default Footer;
