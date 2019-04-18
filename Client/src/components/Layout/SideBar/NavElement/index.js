1d975f951902d4bc54627905eaee9f083725654d
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-18 14:45:30 +0300

    delete commented code

--- Client/src/components/Layout/SideBar/NavElement/index.js
+++ Client/src/components/Layout/SideBar/NavElement/index.js
@@ -6,7 +6,6 @@ export default function NavElement(props) {
   return (
     <NavLink to={link} onClick={handleLinkClick} className='SidBar-link' >
       <img src={icon} alt="icon" className='SideBar-img'/>
-      {/* <i className={icon} ></i> */}
       <h3 className='SideBar-text'>{text}</h3>
     </NavLink>
   )
