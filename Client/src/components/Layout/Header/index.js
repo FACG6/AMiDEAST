be872ca5e745747d44e9d6ab309d858c06cf4444
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-18 11:22:14 +0300

    add the header component with logo and hamburger menu

--- Client/src/components/Layout/Header/index.js
+++ Client/src/components/Layout/Header/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import logo from '../../assets/Image/logoamdieast.png'
+import './index.css'
 
-export default function Header() {
+export default function Header(props) {
+  const { handleMenuClick } = props;
   return (
-    <div>
-      header
+    <div className='Header'>
+      <i className="fas fa-bars , Header-hambrger" onClick={handleMenuClick}></i>
+      <img src={logo} alt="Amideast Logo" className='Header-img' />
     </div>
   )
 }
