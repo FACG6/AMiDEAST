36d049a8791d9440eb81cddc8dbdc3c1aaeedd8b
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-18 11:28:55 +0300

    add the sidebar component fro the mobile layout

--- Client/src/components/Layout/SideBar/index.js
+++ Client/src/components/Layout/SideBar/index.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
+import NavElement from './NavElement'
+import logout from '../../assets/Image/logout.png'
+import course from '../../assets/Image/translator.png'
+import profile from '../../assets/Image/curriculum.png'
+import './index.css'
 
-export default function Sidebar() {
-  return (
-    <div>
-      sidebar
-    </div>
-  )
+export default class SideBar extends Component {
+  state = {
+    studentName: 'Abdallah Ammar'
+  }
+  render() {
+    const {menuOpen, handleLinkClick} = this.props;
+    const { studentName } = this.state;
+    const elements = [
+      {
+        id:0,
+        text: 'Profile',
+        link: 'profile',
+        icon: profile,
+      },
+      {
+        id:1,
+        text: 'All Courses',
+        link: 'courses',
+        icon: course,
+      },
+      {
+        id:2,
+        text: 'Logout',
+        link: 'logout',
+        icon: logout,
+      }
+    ]
+    if (menuOpen) {
+      return (
+        <div className='SideBar'>
+          <div className='SideBar-name'>{studentName}</div>
+          <div>
+            {elements.map(item =>
+              <span key={item.id}>
+                <NavElement link={item.link} text={item.text} icon={item.icon} handleLinkClick={handleLinkClick} />
+              </span>          
+            )}
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        null
+      )
+    }
+  }
 }
