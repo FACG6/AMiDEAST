14d58b0774b31236842888b5c380d221ea314837
Nareman <naremanmohhilles@gmail.com>
2019-04-18 15:32:16 +0300

    solve conflict

--- server/database/config/db_build.js
+++ server/database/config/db_build.js
@@ -1,10 +1,11 @@
-const { readFileSync } = require("fs");
-const path = require("path");
-const connection = require("./db_connection");
+const { readFileSync } = require('fs');
+const path = require('path');
+const connection = require('./db_connection');
 
 const dbBuild = () => {
-  const filepath = path.join(__dirname, "db_build.sql");
+  const filepath = path.join(__dirname, 'db_build.sql');
   const sql = readFileSync(filepath).toString();
   return connection.query(sql);
 };
+
 module.exports = dbBuild;
--- server/database/config/db_connection.js
+++ server/database/config/db_connection.js
@@ -4,21 +4,16 @@ const url = require('url');
 
 let DB_URL = '';
 switch (process.env.NODE_ENV) {
-  case 'test':
-    DB_URL = process.env.DATABASE_TESTING;
-    break;
-  case 'production':
-    DB_URL = process.env.DATABASE_HEROCU;
-    break;
-  case 'dev':
-    DB_URL = process.env.DATABASE_LOCAL;
-    break;
-  default:
-    throw new Error('not found the DB_URL');
+  case 'testing': DB_URL = process.env.DATABASE_TESTING; break;
+  case 'production': DB_URL = process.env.DATABASE_HEROCU; break;
+  case 'development': DB_URL = process.env.DATABASE_LOCAL; break;
+  default: throw new Error('not found the DB_URL');
 }
 
 const params = url.parse(DB_URL);
+
 const [user, password] = params.auth.split(':');
+
 const options = {
   user,
   password,
