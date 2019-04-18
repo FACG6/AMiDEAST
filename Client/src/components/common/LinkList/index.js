3de34438c1adfe94d292ada03c6a5c0a8fc9456f
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-18 15:27:07 +0300

    Edit comment

--- Client/src/components/common/LinkList/index.js
+++ Client/src/components/common/LinkList/index.js
@@ -10,7 +10,7 @@ const sublink = [
 export default function Linklst({ index, link }) {
   return (
     <>
-      <li className='li-links'><i className="fas fa-home icon"></i>{link[0]}</li>
+      <li className='li-links'> <i className="fas fa-home icon"></i>{link[0]}</li>
       <li className='li-links'> <i className="fas fa-certificate icon"></i>{link[1]}
         <ul className='link'>
           {sublink[0].map((link, index) => <Link key={index} link={link} />)}
