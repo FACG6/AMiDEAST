2943ab8eccc5483edc5356b2e00082752d25550f
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-18 15:23:45 +0300

    Edit Identity

--- Client/src/components/common/Link/index.js
+++ Client/src/components/common/Link/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import './index.css'
 
-export default function Link({ link }) { return (<li className='sublink'> {link} </li>) };
+export default function Link({ link }) {
+  return (
+    <li className='sublink'> {link} </li>
+  )
+};
