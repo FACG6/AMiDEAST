0513926c5080a4106791b938fabbbdec5eb1d073
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-20 01:07:48 +0300

    add the profile page for the student

--- Client/src/components/contanier/student/Profile/index.js
+++ Client/src/components/contanier/student/Profile/index.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
+import TextCard from '../../../common/TextCard'
+import './index.css';
 
 export default class Profile extends Component {
+  state = {
+    level: 6,
+  }
+
   render() {
+    const label = ['Name :', 'Phone Number :', 'Amideast ID :']
+    const data = ['Abdallah Amamr', '0597185554', '59763']
     return (
-      <div>
-        from Profile
+      <div className='profile'>
+        <h1 className='profile-titel'>Profile</h1>
+        <h2 className='profile-level' >Level {this.state.level}</h2>
+        {label.map((item, index) => {
+          return (
+            <TextCard data={data[index]} label={item} key={index} />
+          )
+        })}
       </div>
     )
   }
