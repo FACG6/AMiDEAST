96d7ca4b8256a3af700db3b2d20e620cafc43be1
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-20 19:59:17 +0300

    make the input component more useable

--- Client/src/components/common/Label/index.js
+++ Client/src/components/common/Label/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Label = props => {
-  const { input: Input, id, labelText, labelClassName} = props;
+  const { input: Input, id, labelText, labelClassName, name, placeholder, onChange, inputClassName} = props;
   return (
     <label htmlFor={id} className={labelClassName} >
       {labelText}
-      {<Input {...props}/>}
+      {<Input name={name} placeholder={placeholder} onChange={onChange} inputClassName={inputClassName} />}
     </label>
   )
 };
