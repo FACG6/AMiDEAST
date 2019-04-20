34b108f448baf4b4468da2d231229156c6432d4d
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-20 16:37:17 +0300

    start in the login page

--- Client/src/components/contanier/Auth/index.js
+++ Client/src/components/contanier/Auth/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import Button from '../../common/Button'
+import LabeledInput from '../../common/LabeledInput'
+import logo from '../../assets/Image/logoamdieast.png'
+import './index.css'
 
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        login page
-        
+      <div className='login'>
+        <img src={logo} alt="amideast logo" className='login-logo' />  
+        <LabeledInput />
+        <LabeledInput />
+        <Button content='Login' btnClassName='login-btn'/>    
       </div>
     )
   }
