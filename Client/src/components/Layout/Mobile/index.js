e18453eb5990da1600cb710c9d154ec0692ec8b1
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-21 14:13:27 +0300

    just edit the component name

--- Client/src/components/Layout/Mobile/index.js
+++ Client/src/components/Layout/Mobile/index.js
@@ -7,7 +7,7 @@ import Apply from '../../contanier/student/Apply';
 import Profile from '../../contanier/student/Profile';
 import Login from '../../contanier/Auth';
 import SideBar from '../SideBar';
-import MyCourses from '../../contanier/student/Mycourses'
+import StudentCourses from '../../contanier/student/StudentCourses'
 
 export default class Mobile extends Component {
   state = {
@@ -32,7 +32,7 @@ export default class Mobile extends Component {
             <SideBar handleLinkClick={handleLinkClick} menuOpen={menuOpen} />
             <Switch>
               <Route exact path={'/courses'} component={Courses} />
-              <Route exact path={'/mycourses'} component={MyCourses} />
+              <Route exact path={'/mycourses'} component={StudentCourses} />
               <Route exact path={'/apply'} component={Apply} />
               <Route exact path={'/profile'} component={Profile} />
               <Route component={() => <div>404</div>} />
