fa7bc929f4b7623fc24d625c23f2ba174d88359e
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-21 19:37:07 +0300

    edit the course component to make is useable

--- Client/src/components/common/Course/index.js
+++ Client/src/components/common/Course/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import'./index.css'
 
 export default function CoursCard(props) {
-  const { titel, desc } =props
+  const { titel, desc, date, days, time } =props
   return (
     <div className='CoursCard'>
       <h3 className='CoursCard-titel'>{titel}</h3>
       <p className='CoursCard-desc'>{desc}</p>
+      {date ? <h4 className='CoursCard-time' >Start at {date}</h4> : null}
+      {days ? <h4 className='CoursCard-time'>Days : {days}</h4> : null}
+      {time ? <h4 className='CoursCard-time'>Time : {time}</h4> : null}
     </div>
   )
 }
