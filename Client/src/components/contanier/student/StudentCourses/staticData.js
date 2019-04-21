adb037c26e55ebc5f4527eaaf5302008987fa9e5
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-21 19:36:21 +0300

    add a static file for the data

---
+++ Client/src/components/contanier/student/StudentCourses/staticData.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    id:0,
+    titel: 'Writing & Speaking',
+    desc: 'In this course you will learn the fundemtal of speaking and writign skills',
+    date: '1/5/2019',
+    days: 'Sat - Mon - Sun',
+    time: '12 - 3 PM'
+  },
+  {
+    id:1,
+    titel: 'Writing & Speaking',
+    desc: 'In this course you will learn the fundemtal of speaking and writign skills',
+    date: '1/5/2019',
+    days: 'Sat - Mon - Sun',
+    time: '12 - 3 PM'
+  }
+]
+export default data
