308a406fa43017615df177b5f640132f0bc8f8a0
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-23 13:19:46 +0300

    Add Student

---
+++ Client/src/components/contanier/staff/Student/AddStudent/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import LabeledInput from './../../../../common/LabeledInput'
+// import Button from './../../../../common/Button'
+import './index.css';
+
+export default class Addstudent extends Component {
+  render() {
+    return (
+      <div className='addstudent'>
+        <h1 className='addstudent-title'>
+          <span className='addstudent-title-border'>
+            Add Student
+        </span>
+        </h1>
+        <div className='addstudent-contanier'>
+          <div className='addstudent-contanier-left'>
+            <LabeledInput
+              labelText='First name'
+              id='firstname'
+              name='firstname'
+              placeholder='First name '
+            />
+            <LabeledInput
+              labelText='Address'
+              id='address'
+              name='address'
+              placeholder='Address'
+            />
+            <LabeledInput
+              labelText='Level'
+              id='level'
+              name='level'
+              placeholder='Level'
+            />
+            <LabeledInput
+              labelText='Phone number '
+              id='phonenumber'
+              name='phonenumber'
+              placeholder='Phone Number '
+            />
+          </div>
+          <div className='addstudent-contanier-center'></div>
+          <div className='addstudent-contanier-right'>
+            <LabeledInput
+              labelText='Last name'
+              id='lastname'
+              name='lastname'
+              placeholder='Last name '
+            />
+            <LabeledInput
+              labelText='Mobile number'
+              id='mobilenumber'
+              name='mobilenumber'
+              placeholder='Mobile number'
+            />
+            <LabeledInput
+              labelText='Password'
+              id='password'
+              name='password'
+              placeholder='Password'
+            />
+            {/* <Button/> */}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
