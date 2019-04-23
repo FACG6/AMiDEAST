399272a356dae98b44ca386877a08933cd76c6d9
Nareman <naremanmohhilles@gmail.com>
2019-04-23 15:22:28 +0300

    view student component

--- Client/src/components/contanier/staff/Courses/ViewCourse/index.js
+++ Client/src/components/contanier/staff/Courses/ViewCourse/index.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./index.css";
+import Table from "../../../../common/Table";
 
 export default class Viewcourse extends Component {
   render() {
+    const headings = [
+      "Course Name",
+      "Date Of Publish",
+      "Target Level",
+      "Percentage Of Total Numbers"
+    ];
+    const rows = [
+      ["writing and reading", "47/1995", "6", "60%"],
+      ["translating", "4/3/1995", "5", "80%"],
+      ["samerry", "8/9/1995", "3", "70%"],
+      ["sammer", "1/4/1995", "2", "50%"],
+      ["sam", "1/9/1995", "8", "30%"]
+    ];
+
     return (
-      <div>
-        from Viewcourse
-      </div>
-    )
+      <>
+        <h1 className="view-course-titel">
+          <span className="view-course-line ">View Course</span>
+        </h1>
+        <div className="view-course">
+          <Table headings={headings} rows={rows} />
+        </div>
+      </>
+    );
   }
 }
