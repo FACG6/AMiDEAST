2e095af8700df0d4000afd270bfd506811e8356e
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-24 02:54:05 +0300

    require the test files

--- server/test/index.js
+++ server/test/index.js
@@ -1,23 +1,2 @@
-const testFolder = './server/test';
-const fs = require('fs');
-
-fs.readdir(testFolder, (err, files) => {
-  if (err) {
-    console.log(err);
-  } else {
-    files.forEach((file) => {
-      if (file !== 'index.js') {
-        fs.readdir(`${testFolder}/${file}`, (err, files) => {
-          if (err) {
-            console.log(err)
-          } else {
-            console.log(file)
-            files.forEach((subFile) => {
-              require(`./${file}/${subFile}`);
-            });
-          }
-        });
-      };
-    });
-  };
-});
+require('./Endpoints/router-test');
+require('./Queries/query-test');
