74d62122a62d0e599ce55bdf11bbad472f5c32d4
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-23 13:03:01 +0300

    edit style and fix state problem

--- Client/src/components/contanier/student/Apply/index.js
+++ Client/src/components/contanier/student/Apply/index.js
@@ -9,23 +9,25 @@ export default class Apply extends Component {
   state = {
     course: {
       id:0,
-      titel: 'Writing & Speaking',
+      title: 'Writing & Speaking',
       desc: 'In this course you will learn the fundemtal of speaking and writign skills',
       days: ["Pick a day ..", "Sat - Mon - Wedn", "Sun - Tues - Thurs"],
       hours: ["Pick a time ..", "8:00 - 10:00 PM", "10:00 - 12:00 PM", "12:00 - 14:00 AM"]
     }
   }
   render() {
-    const { days, hours } = this.state
+    const { days, hours } = this.state.course
     return (
       <div className='apply'>
-        <h1 className='apply-titel'>Apply</h1>
-        <CourseCard { ...this.state.course }/>
+        <h1 className='apply-title'>Apply</h1>
+        <div className='apply-card'>
+          <CourseCard { ...this.state.course }/>
+        </div>
         <hr className='apply-line' />
         <h4 className='apply-tep'>Select from available options below</h4>
         <PickDay option={days}/>
         <PickDay option={hours}/>
-        <Button btnClassName='apply-btn' content='Apply'/>
+        <Button className='apply-btn' content='Apply'/>
       </div>
     )
   }
