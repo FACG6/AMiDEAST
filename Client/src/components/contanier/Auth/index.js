2679c09ec456f7ef7a3b549262e3c890edb694ea
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-23 14:39:46 +0300

    edit the style

--- Client/src/components/contanier/Auth/index.js
+++ Client/src/components/contanier/Auth/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+
 import Button from '../../common/Button'
 import LabeledInput from '../../common/LabeledInput'
 import logo from '../../assets/Image/amideasblue.png'
+import { firstLable, secondLable } from './staticdata'
 import './index.css'
 
 export default class Login extends Component {
@@ -10,30 +12,13 @@ export default class Login extends Component {
     e.preventdefault()
   }
   render() {
-    const first = {
-      LabeledInputClassName: 'container',
-      id: 'id',
-      labelClassName: 'login-container--label',
-      labelText: 'ID',
-      name: 'id',
-      placeholder: 'Enter your ID ... ',
-      inputClassName: 'login-container--input'
-    };
-    const second = {
-      LabeledInputClassName: 'container',
-      id: 'pass',
-      labelClassName: 'login-container--label',
-      labelText: 'Password',
-      name: 'password',
-      placeholder: 'Enter your password ...',
-      inputClassName: 'login-container--input'
-    }
+
     return (
       <div className='login'>
         <img src={logo} alt="amideast logo" className='login-logo' /> 
-        <div className='container'>
-          <LabeledInput {...first}/>
-          <LabeledInput {...second}/>  
+        <div className='mobile-container'>
+          <LabeledInput {...firstLable}/>
+          <LabeledInput {...secondLable}/>  
         </div> 
         <Button content='Login' className='login-btn' onClick={this.handleClick}/>    
       </div>
---
+++ Client/src/components/contanier/Auth/staticdata.js
@@ -0,0 +1,19 @@
+export const  firstLable = {
+  LabeledInputClassName: 'container',
+  id: 'id',
+  labelClassName: 'login-container--label',
+  labelText: 'ID',
+  name: 'id',
+  placeholder: 'Enter your ID ... ',
+  inputClassName: 'login-container--input'
+};
+
+export const secondLable = {
+  LabeledInputClassName: 'container',
+  id: 'pass',
+  labelClassName: 'login-container--label',
+  labelText: 'Password',
+  name: 'password',
+  placeholder: 'Enter your password ...',
+  inputClassName: 'login-container--input'
+};
