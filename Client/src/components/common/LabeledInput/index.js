70d8c49201b89e7e088a3b2d8b68dfc9642d5082
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-23 12:40:59 +0300

    styling the label input

--- Client/src/components/common/LabeledInput/index.js
+++ Client/src/components/common/LabeledInput/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Input from '../Input';
 import Label from '../Label';
+import './index.css';
 
 const LabeledInput = props => {
   return (
-    <div className={props.LabeledInputClassName}>
+    <div className='LabeledInput'>
       <Label input={Input} {...props} />
     </div>
   )
