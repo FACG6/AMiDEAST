a7ccce6fce7de819a8b74b368c062952469bbb44
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-23 16:52:32 +0300

    Make handleInput and handleClick function

--- Client/src/components/contanier/staff/Student/AddStudent/index.js
+++ Client/src/components/contanier/staff/Student/AddStudent/index.js
@@ -1,43 +1,67 @@
 import React, { Component } from 'react'
 import LabeledInput from './../../../../common/LabeledInput'
 import Button from './../../../../common/Button';
-
+import './logic'
 import './index.css';
 
 export default class AddStudent extends Component {
+  state = {
+    firstname: '',
+    address: '',
+    level: '',
+    phonenumber: '',
+    lastname: '',
+    mobilenumber: '',
+    password: '',
+  }
+
+  handleInput = ({ target: { value, name } }) => this.setState({ [name]: value });
+  handleClick = (e) => {
+    e.preventDefault();
+    // console.log(this.state)
+  }
   render() {
+    const { firstname, address, level, phonenumber, lastname, mobilenumber, password } = this.state;
     return (
       <div className='add-student'>
         <h1 className='add-student-title'>
           <span className='add-student-title-border'>
-            Add  Student                        
+            Add  Student
           </span>
         </h1>
-        <div className='add-student-contanier'>
+        <form onSubmit={(e) => this.handleClick(e)} className='add-student-contanier'>
           <div className='add-student-contanier-left'>
             <LabeledInput
               labelText='First name'
               id='firstname'
-              name='firstname'
+              name='firstname' encodeURIComponent
+              value={firstname}
               placeholder='First name '
+              onChange={this.handleInput}
             />
             <LabeledInput
               labelText='Address'
               id='address'
               name='address'
+              value={address}
               placeholder='Address'
+              onChange={this.handleInput}
             />
             <LabeledInput
               labelText='Level'
               id='level'
               name='level'
+              value={level}
               placeholder='Level'
+              onChange={this.handleInput}
             />
             <LabeledInput
               labelText='Phone number '
               id='phonenumber'
               name='phonenumber'
+              value={phonenumber}
               placeholder='Phone Number '
+              onChange={this.handleInput}
             />
           </div>
           <div className='add-student-contanier-center'></div>
@@ -46,23 +70,29 @@ export default class AddStudent extends Component {
               labelText='Last name'
               id='lastname'
               name='lastname'
+              value={lastname}
               placeholder='Last name '
+              onChange={this.handleInput}
             />
             <LabeledInput
               labelText='Mobile number'
               id='mobilenumber'
               name='mobilenumber'
+              value={mobilenumber}
               placeholder='Mobile number'
+              onChange={this.handleInput}
             />
             <LabeledInput
               labelText='Password'
               id='password'
+              value={password}
               name='password'
               placeholder='Password'
+              onChange={this.handleInput}
             />
-            <Button content='Add' className='add-student-btn'/>
+            <Button type='submit' content='Add' className='add-student-btn' onClick={(e) => this.handleClick(e)} />
           </div>
-        </div>
+        </form>
       </div>
     )
   }
