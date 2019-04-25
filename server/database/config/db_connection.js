953e6ef48495b64a52bca49e5a812b793fffd614
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-25 11:11:06 +0300

    edit the database name

--- server/database/config/db_connection.js
+++ server/database/config/db_connection.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 const url = require('url');
 
 let DB_URL = '';
+
 switch (process.env.NODE_ENV) {
-  case 'testing': DB_URL = process.env.testing_db; break;
+  case 'testing': DB_URL = process.env.TESTING_DB; break;
   case 'production': DB_URL = process.env.DATABASE_HEROCU; break;
-  case 'development': DB_URL = process.env.DATABASE_LOCAL; break;
+  case 'development': DB_URL = process.env.DATABASE_URL; break;
   default: throw new Error('not found the DB_URL');
 }
 
