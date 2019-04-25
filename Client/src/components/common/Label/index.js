554f2747d45739712dcc4a21ff029a318eb52cc7
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-25 09:57:19 +0300

    install yup package

--- Client/src/components/common/Label/index.js
+++ Client/src/components/common/Label/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Label = props => {
-  const { input: Input, id, labelText, labelClassName, name, placeholder, onChange, inputClassName} = props;
+  const { input: Input, id, labelText, labelClassName, name, placeholder, onChange, inputClassName, Error, type } = props;
   return (
     <label htmlFor={id} className={labelClassName} >
       {labelText}
-      <Input name={name} placeholder={placeholder} onChange={onChange} inputClassName={inputClassName} />
+      <Input name={name} id={id} type={type} placeholder={placeholder} onChange={onChange} inputClassName={inputClassName} />
+      <br />
+      <span className='error'>{Error}</span>
     </label>
   )
 };
