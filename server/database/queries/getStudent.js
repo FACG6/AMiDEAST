148daab41d196b44f0b738eb51d0697cabeeed60
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-25 14:33:31 +0300

    edit the query

--- server/database/queries/getStudent.js
+++ server/database/queries/getStudent.js
@@ -1,6 +1,8 @@
 const connection = require('../config/db_connection');
 
-const getStudentInformation = studentId => connection.query(`
-SELECT id, firstname, lastname, mobile_phone, level, is_active FROM student WHERE id = $1;`, [studentId]);
+const getStudentInformation = studentId => connection.query(
+  'SELECT id, firstname, lastname, mobile_phone, level, is_active FROM student WHERE id = $1',
+  [studentId],
+);
 
 module.exports = getStudentInformation;
