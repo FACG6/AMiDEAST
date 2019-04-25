84b21d432fa3ce9efc1a8ab50acc07ad80af214e
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-25 14:43:49 +0300

    edit the test

--- server/test/Queries/query-test.js
+++ server/test/Queries/query-test.js
@@ -7,20 +7,14 @@ test('test query for get student information', (t) => {
   dbBuild().then(() => getStudent(12345))
     .then((res) => {
       if (res.rowCount !== 0) {
-        t.equal(res.rowCount !== 0, true, 'There is student in the database');
-        return res.rows;
-      }
-      t.equal(res.rowCount === 0, true, 'The student is not exist in the database');
-      t.end();
-      return false;
-    })
-    .then((res) => {
-      if (res) {
-        const student = res[0];
+        const student = res.rows[0];
         t.deepEqual(
           Object.keys(student),
           ['id', 'firstname', 'lastname', 'mobile_phone', 'level', 'is_active'], 'Same Data',
         );
+        t.end();
+      } else {
+        t.equal(res.rowCount === 0, true, 'The student is not exist in the database');
         t.end();
       }
     })
