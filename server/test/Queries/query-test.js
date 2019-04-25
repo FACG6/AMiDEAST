12b2937f0f7ece95597c4275d3deb1ae1205abb9
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-25 03:56:09 +0300

    add the test for the query

--- server/test/Queries/query-test.js
+++ server/test/Queries/query-test.js
@@ -1,6 +1,33 @@
-const tape = require('tape');
+const test = require('tape');
 
-tape('just for testing', (t) => {
-  t.equal(1, 1, 'on is equal one');
-  t.end();
+const getStudent = require('../../database/queries/getStudent');
+
+test('test query for get student information', (t) => {
+  getStudent(12345)
+    .then((res) => {
+      if (res.rowCount !== 0) {
+        t.equal(res.rowCount !== 0, true, 'There is student in the database');
+        return res.rows;
+      }
+      t.equal(res.rowCount === 0, true, 'The student is not exist in the database');
+      t.end();
+      return false;
+    })
+    .then((res) => {
+      if (res) {
+        const student = res[0];
+        t.deepEqual(
+          Object.keys(student),
+          ['amideastid', 'firstname', 'lastname', 'mobilephone', 'level', 'isactive'], 'Same Data',
+        );
+        t.end();
+      }
+    })
+    .catch((error) => {
+      t.error(error);
+    });
+});
+
+test.onFinish(() => {
+  process.exit(0);
 });
