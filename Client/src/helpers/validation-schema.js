6112f845a6fbd5a17184455d3dd3e5537e3dbb1a
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-25 14:28:52 +0300

    Edit validation-schema

--- Client/src/helpers/validation-schema.js
+++ Client/src/helpers/validation-schema.js
@@ -1,14 +1,13 @@
 const yup = require('yup');
 
 let addStudentSchema = yup.object().shape({
-  firstname: yup.string().min(3).max(15).required('First name must be at least 3 characters'),
-  address: yup.string().min(3).max(50).required('Address must be at least 3 characters'),
-  level: yup.number().lessThan(13).moreThan(0).required('level must be greater than 0'),
-  phonenumber: yup.string().min(3).max(50).required('phonenumber must be at least 3 characters'),
-  lastname: yup.string().min(3).max(50).required('lastnem must be at least 3 characters'),
-  mobilenumber: yup.string().min(3).max(50).required('mobilenumber must be at least 3 characters'),
-  password: yup.string().min(5).max(50).required('password must be at least 3 characters'),
+  firstname: yup.string().min(3).max(15),
+  address: yup.string().min(3).max(50),
+  level: yup.number().lessThan(13).moreThan(0),
+  phonenumber: yup.string().min(3).max(50),
+  lastname: yup.string().min(3).max(50),
+  mobilenumber: yup.string().min(3).max(50),
+  password: yup.string().min(5).max(50)
 });
-
 
 export { addStudentSchema };
