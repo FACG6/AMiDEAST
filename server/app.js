17724ae348d846a26ab99fd651b921a4c42458fa
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-26 22:52:56 +0300

    add cookie-parses

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
 const app = express();
 app.disable('x-powered-by');
 app.use(express.json());
+app.use(cookieParser());
 app.set('port', process.env.PORT || 7777);
-
 module.exports = app;
