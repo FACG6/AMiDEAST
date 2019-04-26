cf7cd0adb6b2538afa66a483b8c232174ce73887
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-26 20:59:04 +0300

    make the test for the profile route

--- server/test/Endpoints/router-test.js
+++ server/test/Endpoints/router-test.js
@@ -1,6 +1,30 @@
-const tape = require('tape');
+const test = require('tape');
+const request = require('supertest');
+const app = require('../../app');
+// const connection = require('../../database/config/db_connection');
 
-tape('just for testing', (t) => {
-  t.equal(1, 1, 'on is equal one');
-  t.end();
+// const selectFirstUser = () => connection.query('select id from student LIMIT 1');
+
+test('Get student information from /api/v1/student/user/12345', (t) => {
+  // selectFirstUser()
+  //   .then(res => console.log(res.rows[0].id))
+  //   .catch(err =>console.log(22222, err))
+  request(app)
+    .get('/api/v1/student/user/12345')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      const obj = JSON.parse(res.text);
+      if (err) {
+        t.error(err);
+      } else if (obj.error) {
+        t.error(obj.error);
+      } else {
+        t.deepEqual(Object.keys(obj.data[0]), ['id', 'firstname', 'lastname', 'mobile_phone', 'level', 'is_active'], 'get same data that expected');
+        t.equal(obj.data[0].id, 12345, 'User ID for the correct');
+        t.end();
+      }
+    });
 });
+
+test.onFinish(() => process.exit(0));
