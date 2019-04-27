21a560a95686018d96c02e1f655f8a295c464ad5
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-27 16:09:56 +0300

    edit the test

--- server/test/Endpoints/router-test.js
+++ server/test/Endpoints/router-test.js
@@ -90,6 +90,53 @@ test('Get when no available courses for the student', (t) => {
     });
 });
 
+test('Get all applied courses for the student from /api/v1/student/course/mycourse', (t) => {
+  dbBuild()
+    .then(() => {
+      request(app)
+        .get('/api/v1/student/course/mycourses')
+        .send({ id: 12345 })
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          const obj = JSON.parse(res.text);
+          if (err) {
+            t.error(err);
+          } else {
+            t.deepEqual(Object.keys(obj.data[0]), ['title', 'description', 'publish_date', 'days', 'h_from', 'h_to'], 'get Same data expected');
+            t.equal(obj.data[0].title, 'grammer', 'Course title is correct');
+            t.end();
+          }
+        });
+    })
+    .catch((err) => {
+      t.error(err);
+    });
+});
+
+test('Get all applied courses for the student dose not exist from /api/v1/student/course/mycourse', (t) => {
+  dbBuild()
+    .then(() => {
+      request(app)
+        .get('/api/v1/student/course/mycourses')
+        .send({ id: 125 })
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          const obj = JSON.parse(res.text);
+          if (err) {
+            t.error(err);
+          } else if (obj.error) {
+            t.equal(obj.error, 'No courses are applied for this student', 'Course title is correct');
+            t.end();
+          }
+        });
+    })
+    .catch((err) => {
+      t.error(err);
+    });
+});
+
 test('Post new course for the student at /api/v1/student/course/applycourse', (t) => {
   dbBuild()
     .then(() => {
