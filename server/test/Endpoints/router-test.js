9b134cb34109c4a3af0c7757ae5c05652dc0f1c6
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-27 13:58:27 +0300

    edit the test

--- server/test/Endpoints/router-test.js
+++ server/test/Endpoints/router-test.js
@@ -14,11 +14,29 @@ test('Get student information from /api/v1/student/user/12345', (t) => {
           const obj = JSON.parse(res.text);
           if (err) {
             t.error(err);
-          } else if (obj.error) {
-            t.error(obj.error);
           } else {
             t.deepEqual(Object.keys(obj.data[0]), ['id', 'firstname', 'lastname', 'mobile_phone', 'level', 'is_active'], 'get same data that expected');
             t.equal(obj.data[0].id, 12345, 'User ID for the correct');
+            t.end();
+          }
+        });
+    })
+    .catch(err => t.error(err));
+});
+
+test('Get user dose not exist from  /api/v1/student/user/123456', (t) => {
+  dbBuild()
+    .then(() => {
+      request(app)
+        .get('/api/v1/student/user/123456')
+        .expect(404)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          const obj = JSON.parse(res.text);
+          if (err) {
+            t.error(err);
+          } else if (obj.error) {
+            t.equal(obj.error, 'The student dose not exist', 'get same error that expected');
             t.end();
           }
         });
@@ -31,6 +49,30 @@ test('Get all available courses for the student', (t) => {
     .then(() => {
       request(app)
         .get('/api/v1/student/course/allcourses')
+        .send({ level: 2 })
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          const obj = JSON.parse(res.text);
+          if (err) {
+            t.error(err);
+          } else {
+            t.deepEqual(Object.keys(obj.data[0]), ['title', 'description'], 'get same data');
+            t.end();
+          }
+        });
+    })
+    .catch((err) => {
+      t.error(err);
+    });
+});
+
+test('Get when no available courses for the student', (t) => {
+  dbBuild()
+    .then(() => {
+      request(app)
+        .get('/api/v1/student/course/allcourses')
+        .send({ level: 20 })
         .expect(200)
         .expect('Content-Type', /json/)
         .end((err, res) => {
@@ -38,10 +80,7 @@ test('Get all available courses for the student', (t) => {
           if (err) {
             t.error(err);
           } else if (obj.error) {
-            t.error(err);
-          } else {
-            t.deepEqual(Object.keys(obj.data[0]), ['title', 'description'], 'get Same data expected');
-            t.equal(obj.data[0].title, 'grammer', 'Course title is correct');
+            t.equal(obj.error, 'No courses available for this level', 'get Same error expected');
             t.end();
           }
         });
@@ -61,8 +100,6 @@ test('Get all applied courses for the student from /api/v1/student/course/mycour
         .end((err, res) => {
           const obj = JSON.parse(res.text);
           if (err) {
-            t.error(err);
-          } else if (obj.error) {
             t.error(err);
           } else {
             t.deepEqual(Object.keys(obj.data[0]), ['title', 'description', 'publish_date', 'days', 'h_from', 'h_to'], 'get Same data expected');
