3ea1899f809ab985ab9b4fe4bf4b7df6985763f0
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-27 14:38:37 +0300

    delete commented code

--- server/controllers/student/index.js
+++ server/controllers/student/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 
 const user = require('./user');
-// const course = require('./courses');
 
 router.use('/user', user);
-// router.use('/course', course);
 
 module.exports = router;
