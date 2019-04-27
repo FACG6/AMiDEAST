cdb8565bb23d9e433255a2cdc9df8cdc8a59cdce
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-27 11:28:17 +0300

    edit the app and add the express.urlencoded

--- server/app.js
+++ server/app.js
@@ -6,12 +6,12 @@ const app = express();
 app.disable('x-powered-by');
 app.use(express.json());
 app.set('port', process.env.PORT || 7777);
-
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, '..', 'Client', 'build')));
 app.use('/api/v1', router);
 
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, '..', 'CLient', 'build', 'index.html'));
+  res.sendFile(join(__dirname, '..', 'Client', 'build', 'index.html'));
 });
 
 module.exports = app;
