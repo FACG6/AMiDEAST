2f2f2eec5c2ede3b2f51410e342c0448c48fe531
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-29 02:37:49 +0300

    edit the query for add student and course

--- server/database/queries/addCourse.js
+++ server/database/queries/addCourse.js
@@ -1,7 +1,7 @@
 const connection = require('../config/db_connection');
 
 const insertCourse = (courseInfo) => {
-  const queryValues = Object.keys(courseInfo).map(info => courseInfo[info]);
+  const queryValues = Object.values(courseInfo);
   const sql = {
     text: 'INSERT INTO course(title, description, target_level, number_of_student, publish_date) values ($1, $2, $3, $4, $5)  RETURNING *',
     values: [...queryValues],
--- server/database/queries/addStudent.js
+++ server/database/queries/addStudent.js
@@ -1,7 +1,7 @@
 const connection = require('../config/db_connection');
 
 const insertStudent = (userInfo) => {
-  const queryValues = Object.keys(userInfo).map(info => userInfo[info]);
+  const queryValues = Object.values(userInfo);
   const sql = {
     text: 'INSERT INTO student(password, mobile_phone, lastname, home_phone, level, address , firstname) values ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
     values: [...queryValues],
