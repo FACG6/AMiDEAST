297bfb099f396bc4b130b172b901da6f30e64e3b
Nareman <naremanmohhilles@gmail.com>
2019-04-28 12:17:03 +0300

    solve conflict

--- server/test/Endpoints/router-test.js
+++ server/test/Endpoints/router-test.js
@@ -9,25 +9,27 @@ test('just for testing', (t) => {
 });
 
 test('add course courses', (t) => {
-  request(app)
-    .post('/api/v1/staff/course/addcourse')
-    .send({
-      courseName: 'grammer',
-      description: 'this courrse',
-      level: 8,
-      numberofstudent: 29,
-      publishDates: '14/4/2014',
-    })
-    .expect(200)
-    .expect('Content-Type', 'application/json; charset=utf-8')
-    .end((errr, res) => {
-      if (errr) {
-        t.error(errr);
-      } else {
-        console.log(res.body);
-        t.equal(res.body.data[0].title, 'grammer', 'the title must be grammer');
-        t.end();
-      }
+  dbBuild()
+    .then(() => {
+      request(app)
+        .post('/api/v1/staff/course/addcourse')
+        .send({
+          courseName: 'grammer',
+          description: 'this courrse',
+          level: 8,
+          numberofstudent: 29,
+          publishDates: '14/4/2014',
+        })
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .end((errr, res) => {
+          if (errr) {
+            t.error(errr);
+          } else {
+            t.equal(res.body.data[0].title, 'grammer', 'the title must be grammer');
+            t.end();
+          }
+        });
     });
 });
 
