93f0033710bc25c0c60d010bf9fca063e592aee2
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-29 14:06:23 +0300

    edit add course

--- server/controllers/courses/addCourse.js
+++ server/controllers/courses/addCourse.js
@@ -3,17 +3,19 @@ const { insertCourse, insertDates } = require('../../database/queries/addCourse'
 const { addCourseSchema } = require('../../helpers/validation-schema');
 
 exports.addCourse = (req, res) => {
-  const { error } = joi.validate({ ...req.body }, addCourseSchema);
+  const {
+    title, description, level, numberOfStudent, start, end, days,
+  } = req.body;
+  const couresInofrmation = {
+    title, description, level, numberOfStudent, start, end, days,
+  };
+  const { error } = joi.validate(couresInofrmation, addCourseSchema);
   if (error) {
     res.send({ msg: error.details[0].message });
   } else {
-    const {
-      title, description, level, numberOfStudent,
-    } = req.body;
     const courseInfo = {
       title, description, level, numberOfStudent,
     };
-    const { start, end, days } = req.body;
     const datesInfo = { start, end, days };
     insertCourse(courseInfo)
       .then(({ rows: course }) => {
