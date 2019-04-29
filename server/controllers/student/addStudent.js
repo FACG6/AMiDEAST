230b31d6c8c15cd39ad0d391ea6bec366a2cc8ca
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-29 12:41:18 +0300

    student route

--- server/controllers/student/addStudent.js
+++ server/controllers/student/addStudent.js
@@ -1,24 +1,26 @@
+const joi = require('joi');
 const { insertStudent } = require('../../database/queries/addStudent');
+const { studentSchema } = require('../../helpers/validation-schema');
 
 const addStudent = (req, res) => {
-  insertStudent({ ...req.body })
-    .then((student) => {
-      if (!student.rowCount) {
-        res.status(400).send({
-          error: 'Student dose not added',
-          data: null,
-        });
-      } else {
-        res.status(201).send({
-          error: null,
-          data: student.rows,
-        });
-      }
-    })
-    .catch(() => res.status(500).send({
-      error: 'Sorry we have some issues',
-      data: null,
-    }));
+  const {
+    firstname, lastname, phonenumber, mobilenumber, address, level, password,
+  } = req.body;
+  const userInfo = {
+    firstname, lastname, phonenumber, mobilenumber, address, level, password,
+  };
+
+  const { error } = joi.validate({ ...req.body }, studentSchema);
+  if (error) {
+    res.send({ msg: error.details[0].message });
+  } else {
+    insertStudent(userInfo)
+      .then(({ rows: student }) => {
+        if (!student[0]) res.status(400).send({ error: 'Student dose not added' });
+        else res.status(201).send({ data: student[0] });
+      })
+      .catch(() => res.status(500).send({ error: 'Sorry we have some issues' }));
+  }
 };
 
 module.exports = { addStudent };
