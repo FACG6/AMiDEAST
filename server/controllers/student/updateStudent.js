b42bdece9181d88b2134ba0aa55ab48b5d714b19
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-29 13:09:01 +0300

    make validation for edit student

--- server/controllers/student/updateStudent.js
+++ server/controllers/student/updateStudent.js
@@ -1,23 +1,29 @@
+const joi = require('joi');
 const { putStudent } = require('../../database/queries/updateStudent');
+const { studentSchema } = require('../../helpers/validation-schema');
 
 exports.updateStudent = (req, res) => {
-  const { id } = req.params;
-  putStudent(req.body, id)
-    .then((updatedStudent) => {
-      if (!updatedStudent.rowCount) {
-        res.status(404).send({
-          error: 'student not found ',
-          data: null,
-        });
-      } else {
-        res.status(202).send({
-          error: null,
-          data: 'Updated successfully',
-        });
-      }
-    })
-    .catch(() => res.status(500).send({
-      error: 'Sorry we have some issues',
-      data: null,
-    }));
+  const {
+    firstname, lastname, phonenumber, mobilenumber, address, level, password,
+  } = req.body;
+  const userInfo = {
+    firstname, lastname, phonenumber, mobilenumber, address, level, password,
+  };
+
+  const { error } = joi.validate(userInfo, studentSchema);
+  if (error) {
+    res.send({ msg: error.details[0].message });
+  } else {
+    const { id } = req.params;
+    putStudent(req.body, id)
+      .then((updatedStudent) => {
+        if (!updatedStudent.rowCount) {
+          res.status(404).send({ error: 'student not found ' });
+        } else res.status(202).send({ data: 'Updated successfully' });
+      })
+      .catch(() => res.status(500).send({
+        error: 'Sorry we have some issues',
+        data: null,
+      }));
+  }
 };
