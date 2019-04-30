4fa3e2a5998e6bc2e120ad8ce27afe1a91adcd06
Nareman <naremanmohhilles@gmail.com>
2019-04-30 11:55:24 +0300

    put component state

--- Client/src/components/contanier/staff/Courses/ViewCourse/index.js
+++ Client/src/components/contanier/staff/Courses/ViewCourse/index.js
@@ -1,31 +1,31 @@
 import React, { Component } from "react";
 import "./index.css";
 import Table from "../../../../common/Table";
-import Button from "../../../../common/Button";
 
 export default class Viewcourse extends Component {
-  render() {
-    const headings = [
+  state = {
+    headings: [
       "Course Name",
       "Date Of Publish",
       "Target Level",
       "Percentage Of Total Numbers"
-    ];
-    const rows = [
+    ],
+    rows: [
       ["writing and reading", "47/1995", "6", "60%"],
       ["translating", "4/3/1995", "5", "80%"],
       ["samerry", "8/9/1995", "3", "70%"],
       ["sammer", "1/4/1995", "2", "50%"],
       ["sam", "1/9/1995", "8", "30%"]
-    ];
-
+    ],
+  }
+  render() {
     return (
       <>
         <h1 className="view-course-titel">
           <span className="view-course-line">View Course</span>
         </h1>
         <div className="view-course">
-          <Table headings={headings} rows={rows} />
+          <Table headings={this.state.headings} rows={this.state.rows} />
         </div>
       </>
     );
