a5865fdc6e122aa581937c6e0325aedc9eecf496
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-05-02 11:19:14 +0300

    edit

--- server/database/queries/getCoursesLevel.js
+++ server/database/queries/getCoursesLevel.js
@@ -1,6 +1,6 @@
 const connection = require('../config/db_connection');
 
 const getCoursesByLevel = level => connection.query(`
-SELECT * FROM course WHERE target_level = $1`, [level]);
+SELECT * FROM course inner join dates on dates.course_id = course.id WHERE target_level = $1`, [level]);
 
 module.exports = getCoursesByLevel;
