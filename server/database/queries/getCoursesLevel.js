ebbed83793f8f994f3b3363dee47181214139479
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-05-02 12:27:15 +0300

    edit the path

--- server/database/queries/getCoursesLevel.js
+++ server/database/queries/getCoursesLevel.js
@@ -3,4 +3,12 @@ const connection = require('../config/db_connection');
 const getCoursesByLevel = level => connection.query(`
 SELECT * FROM course inner join dates on dates.course_id = course.id WHERE target_level = $1`, [level]);
 
-module.exports = getCoursesByLevel;
+const avaliableCourses = (level, studentId) => connection.query(`
+select * from course
+inner join student_course on course.id = student_course.course_id 
+where course.target_level = ${1}
+and student_course.student_id = ${2} 
+and course.id != student_course.course_id`, [level, studentId]);
+
+
+module.exports = { getCoursesByLevel, avaliableCourses};
--- server/database/queries/getCoursesStudent.js
+++ server/database/queries/getCoursesStudent.js
@@ -8,12 +8,4 @@ INNER JOIN student ON student.id = student_course.student_id
 INNER JOIN dates ON dates.id = student_course.dates_id
 WHERE student.id = $1`, [studentId]);
 
-const avaliableCourses = (level, studentId) => connection.query(`
-select * from course
-inner join student_course on course.id = student_course.course_id 
-where course.target_level = ${1}
-and student_course.student_id = ${2} 
-and course.id != student_course.course_id`, [level, studentId]);
-
-
-module.exports = { getStudentCourses, avaliableCourses };
+module.exports = getStudentCourses;
