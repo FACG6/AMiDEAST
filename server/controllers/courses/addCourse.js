4f8c1923bb547e15fcd8f34f3cb8f4e61edc840b
Fatma Siam <fatma.o.siam@gmail.com>
2019-05-02 12:19:18 +0300

    edit add course controllers

--- server/controllers/courses/addCourse.js
+++ server/controllers/courses/addCourse.js
@@ -5,13 +5,7 @@ const { addCourseSchema } = require('../../helpers/validation-schema');
 
 exports.addCourse = (req, res) => {
   const {
-    title,
-    description,
-    level,
-    numberOfStudent,
-    start,
-    end,
-    days,
+    title, description, level, numberOfStudent, start, end, days,
   } = req.body.value;
   const { dates } = req.body.value;
   const courseInformation = {
@@ -25,29 +19,22 @@ exports.addCourse = (req, res) => {
     title, description, level, numberOfStudent, start, end, days,
   }, addCourseSchema);
   if (error) {
-    console.log(error);
-    res.status(400).send({
-      msg: error.details[0].message,
-      data: null,
-    });
+    res.status(400).send({ msg: error.details[0].message, data: null });
   } else {
     insertCourse(courseInformation.courseInfo)
       .then(({ rows: course }) => {
-        console.log(course);
         if (!course[0]) throw new Error({ internalError: 'Bad Request' });
         else {
-          return courseInformation.datesInfo.map((date) => {
-            console.log(date.start);
-            insertDates({ start: date.start, end: date.end, days: date.days }, course[0].id);
-          });
+          return courseInformation.datesInfo.map(date => insertDates({ start: date.start, end: date.end, days: date.days }, course[0].id)
+            .then(({ rows: courseDates }) => {
+              if (!courseDates[0]) throw new Error({ internalError: 'Bad Request' });
+              else res.status(201).send({ data: 'Course added successfuly ' });
+            })
+            .catch(({ internalError }) => {
+              if (internalError) res.status(500).send({ error: internalError });
+              else res.status(500).send({ error: 'internal server error' });
+            }));
         }
-      })
-      .then(({ rows: datesinfo }) => {
-        console.log(1111111, datesinfo[0]);
-        if (!datesinfo[0]) throw new Error({ internalError: 'Bad Request' });
-        return res.status(201).send({
-          data: courseInformation,
-        });
       })
       .catch(({ internalError }) => {
         if (internalError) res.status(500).send({ error: internalError });
