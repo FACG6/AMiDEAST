7706b0d1c66835ae2e1b7f4b52305473099ba4ec
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-05-08 11:41:45 +0300

    edit the

--- Client/src/components/Layout/Mobile/index.js
+++ Client/src/components/Layout/Mobile/index.js
@@ -1,27 +1,18 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import axios from "axios";
 
-import Header from "./../Header";
 import Footer from "./../MobileFooter";
 import Courses from "../../contanier/student/Coures";
 import Apply from "../../contanier/student/Apply";
 import Profile from "../../contanier/student/Profile";
-import SideBar from "../SideBar";
 import StudentCourses from "../../contanier/student/StudentCourses";
 import auth from "./../../auth";
+import SideNav from '../SideNav'
 
 export default class Mobile extends Component {
   state = {
-    menuOpen: false,
     level: "",
     courseId: null
-  };
-  handleLinkClick = () => {
-    this.setState({ menuOpen: false });
-  };
-  handleMenuClick = () => {
-    this.setState({ menuOpen: !this.state.menuOpen });
   };
 
   componentDidMount() {
@@ -31,16 +22,11 @@ export default class Mobile extends Component {
 
   render() {
     const { id } = auth.isAuthenticated();
-    const { menuOpen } = this.state;
-    const { handleMenuClick, handleLinkClick } = this;
     const { role } = auth.isAuthenticated();
     if (role === "staff") this.props.history.push("/staff");
     return (
       <>
-        <Header handleMenuClick={handleMenuClick} menuOpen={menuOpen} />
-        <SideBar
-          handleLinkClick={handleLinkClick}
-          menuOpen={menuOpen}
+        <SideNav 
           history={this.props.history}
         />
         <Switch>
---
+++ Client/src/components/Layout/SideNav/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+import SideBar from '../SideBar'
+import Header from '../Header'
+
+export default class SideNav extends Component {
+
+  state = {
+    menuOpen: false,
+  }
+
+  handleLinkClick = () => {
+    this.setState({ menuOpen: false });
+  };
+
+  handleMenuClick = () => {
+    this.setState({ menuOpen: !this.state.menuOpen });
+  };
+
+  render() {
+
+    const { menuOpen } = this.state;
+    const { handleMenuClick, handleLinkClick } = this;
+
+    return (
+      <div>
+        <Header handleMenuClick={handleMenuClick} menuOpen={menuOpen} />
+        <SideBar
+          handleLinkClick={handleLinkClick}
+          menuOpen={menuOpen}
+          history={this.props.history}
+        />
+
+      </div>
+    )
+  }
+}
